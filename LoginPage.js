d88dfec3e21fbcbef5fda26c716c4d1d148914e8
Siddharth Paratkar <Aero@Siddharths-MBP.fios-router.home>
2020-07-06 23:46:53 -0400

    Style changes to LoginPage

--- LoginPage.js
+++ LoginPage.js
@@ -4,18 +4,26 @@ import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#add8e6',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20
   },
   login: {
-    fontSize: 50,
+    fontSize: 40,
     fontWeight: "bold",
+    marginBottom: '100%',
+    
   },
   input: {
+    height: 40,
     padding: 5,
     borderColor: "black",
     borderWidth: 1,
+    marginTop: 5,
+    marginBottom: 20,
+    width: '90%',
+    color: "#000",
   }
 });
 
@@ -25,23 +33,27 @@ export default class LoginPage extends React.Component {
     password: "",
   }
 
-  render() {
+  render() { //add logo if/when we make one
     return (
       <View style={styles.container}>
-        <Text style={styles.login}>Login</Text>
-        <TextInput 
+        <Text //change color + font
+          style={styles.login}>Welcome to CourseMate!
+        </Text>
+        <TextInput //email validation
           style={styles.input}
-          placeholder="Username"
+          placeholder="Email"
+          placeholderTextColor='#000'
           onChangeText={username => this.setState({username})}
           value={this.state.username}
         />
         <TextInput
           style={styles.input}
           placeholder="Password"
+          placeholderTextColor='#000'
           onChangeText={password => this.setState({password})}
           value={this.state.password}
         />
-        <Button title="Log in"/>
+        <Button title="Log in" color='#E7821E'/>
       </View>
     );
   }
